ad74d1b1f05f28900e596299cb9c553b3332fb6f
Litery <s.litera@gmail.com>
2019-04-04 13:45:45 +0200

    Use redis connection pool

--- betbright/application/config.py
+++ betbright/application/config.py
@@ -1,6 +1,6 @@
 from os import environ
 
-from aioredis import Redis, create_redis
+from aioredis import Redis, create_redis_pool
 from injector import Module
 
 REDIS_URL = environ.get('REDIS_URL', 'redis://localhost')
@@ -8,6 +8,7 @@ REDIS_URL = environ.get('REDIS_URL', 'redis://localhost')
 
 class BaseModule(Module):
     async def setup_aio_providers(self, injector):
-        redis = await create_redis(REDIS_URL)
+        redis = await create_redis_pool(
+            REDIS_URL, minsize=5, maxsize=10)
 
         injector.binder.bind(Redis, to=redis)
